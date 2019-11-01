272e5406bf88a7f11876c116e579c3b050e859dc
thembones79 <michal.szumnarski@gmail.com>
2019-11-01 22:30:22 +0100

    refactored functional to class component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return (
-    <div>
-      Hi !!! <SeasonDisplay />
-    </div>
-  );
-};
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position.coords.latitude);
+        let dupa = position.coords.latitude;
+        return dupa;
+      },
+      err => console.log(err)
+    );
+
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
